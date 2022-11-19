4d5ab1045b48dd3f9d49c01f9ac8a94d3a205f28
ojpro <contact@ojpro.me>
2022-11-20 00:54:44 +0100

    ✅ Added Request validation for creating new books.

---
+++ app/Http/Requests/StoreFileRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\File;
+
+class StoreFileRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            "title" => ["required", "string", "min:5", "unique:books,title"],
+            "description" => ["required", "string", "min:20"],
+            "file" => ["required", File::types(['pdf', 'epub', 'mobi'])->max(50 * 1024)]
+        ];
+    }
+}
