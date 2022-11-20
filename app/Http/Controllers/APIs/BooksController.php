0504a444e8c25514feb5a746ecb40516bc88e239
ojpro <contact@ojpro.me>
2022-11-20 14:45:53 +0100

    Documentation ^_^.

--- app/Http/Controllers/APIs/BooksController.php
+++ app/Http/Controllers/APIs/BooksController.php
@@ -59,8 +59,10 @@ class BooksController extends Controller
      */
     public function show(Book $book)
     {
+        // find the book
         $book = Book::findOrFail($book["id"])->first();
 
+        // return it
         return response()->json($book);
     }
 
