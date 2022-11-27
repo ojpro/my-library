eff5c75959f124c7e823cf6ce613f6aeb009b253
ojpro <contact@ojpro.me>
2022-11-27 23:55:31 +0100

    Added search API endpoint.

--- routes/api.php
+++ routes/api.php
@@ -8,10 +8,6 @@ Route::controller(BooksController::class)->prefix("books")->name("api.books.")->
 
     Route::get("/", "index")->name("index");
 
-    Route::get("/?search={query}&category={type}", function ($query, $category) {
-        return $query . "  ====  " . $category;
-    })->name("search");
-
     Route::post("/", "store")->name("store");
 
     Route::get("/{book}", "show")->name("show");
