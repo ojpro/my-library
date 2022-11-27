fa97524aab016951d1469d3fb50569bf822b94b8
ojpro <contact@ojpro.me>
2022-11-27 23:59:52 +0100

    ✅ Added search component.

--- resources/js/Components/Form/SearchField.vue
+++ resources/js/Components/Form/SearchField.vue
@@ -51,9 +51,9 @@
       </div>
       <div class="relative w-full">
         <input id="search-dropdown"
+               v-model="inputValue"
                class="block p-4 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
-               placeholder="Search Programming, Self-Dev, Electronics..."
-               required type="search">
+               placeholder="Search Programming, Self-Dev, Electronics..." required type="search" @keyup="typing">
         <button
             class="absolute top-0 right-0 p-4 text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             type="submit">
@@ -71,7 +71,18 @@
 
 <script>
 export default {
-  name: "SearchField"
+  name: "SearchField",
+  data() {
+    return {
+      inputValue: ""
+    }
+  },
+  methods: {
+    typing() {
+      // pass input data to the parent
+      this.$emit("searchFor", this.inputValue)
+    }
+  }
 }
 </script>
 
---
+++ resources/js/Pages/Search.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container mx-auto">
+    <div class="w-full sm:w-4/5 md:w-2/3 lg:w-1/2 mx-auto p-4">
+      <SearchField @searchFor="searchValue"></SearchField>
+    </div>
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 container mx-auto mt-12">
+      <BookCard v-for="book in this.books" :key="book.id" :description="book.description"
+                :title="book.title"></BookCard>
+    </div>
+  </div>
+</template>
+
+<script>
+import SearchField from "@/Components/Form/SearchField.vue";
+import BookCard from "@/Components/BookCard.vue";
+
+import axios from "axios";
+
+export default {
+  name: "Search",
+  components: {SearchField, BookCard},
+  data() {
+    return {
+      books: {}
+    }
+  },
+  mounted() {
+    this.searchValue()
+  },
+  methods: {
+    searchValue: async function (value = '') {
+      // search for the received data
+      await axios.get(`/api/books/?query=${value}`)
+          // TODO
+          .then(({data}) => {
+            this.books = data
+          })
+          // catch response errors
+          .catch(({response}) => {
+            // TODO
+            console.log(response)
+          })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
